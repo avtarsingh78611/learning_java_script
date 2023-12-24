07eb965839ce34e5c7a1a9efe958dc18a42f58e8
Avtar Singh <avtarsingh78611@gmail.com>
2023-12-24 13:44:54 +0000

    Conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,12 @@
+let score = 1
+console.log(typeof score);
+console.log(typeof(score));
+//console.log(score);
+
+let valueInNumber = Boolean(score)
+console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log("The value of stringnumber is ",stringnumber, "and its type is: ", typeof stringnumber);
